dfa66e8894cf553678e3eb5550e4b2b3c77faa16
donikamarkowa <donimarkova@gmail.com>
2024-03-07 17:18:45 +0200

    Student class added

---
+++ VersionControlAndGit/VersionControlAndGit/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionControlAndGit
+{
+    internal class Student
+    {
+        public string? Name { get; set; }
+    }
+}
