c7ed2fb59180f955ad4240c0416338bbd66c8a65
donikamarkowa <donimarkova@gmail.com>
2024-03-22 10:59:51 +0200

    Add class Person

---
+++ FilesystemAndStreams/Assignment/Person.cs
@@ -0,0 +1,34 @@
+﻿using Assignment.Interfaces;
+
+namespace Assignment
+{
+    internal class Person
+    {
+        private readonly ILogger _logger;
+        public Person(string name, int age, string phoneNumber, ILogger logger)
+        {
+            this.Name = name;
+            this.Age = age; 
+            this.PhoneNumber = phoneNumber;
+            _logger = logger;
+        }
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public async Task ChangePhoneNumber(string newPhoneNumber)
+        {
+            try
+            {
+                await _logger.LogAsync(nameof(ChangePhoneNumber), true);
+                this.PhoneNumber = newPhoneNumber;
+            }
+            catch (Exception)
+            {
+                await _logger.LogAsync(nameof(ChangePhoneNumber), false);
+                throw;
+            }
+               
+        }
+    }
+}
