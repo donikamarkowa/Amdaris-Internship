edac71ee70b5ab689e46a78c49583ceba9b358f2
donikamarkowa <donimarkova@gmail.com>
2024-03-24 22:28:05 +0200

    Add dbContext

---
+++ CleanCode/Assignment/DbContext/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Assignment.Entities;
+
+namespace Assignment.DbContext
+{
+    public class DbContext<T> where T : Entity
+    {
+        private List<T> _values;
+        public DbContext()
+        {
+            _values = new List<T>();
+        }
+
+        public List<T> Values => _values;
+    }
+}
