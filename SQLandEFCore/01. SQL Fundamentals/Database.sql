35aa7cbc821f812e77b6da7f778c3cf544928635
donikamarkowa <donimarkova@gmail.com>
2024-04-05 11:45:55 +0300

    Add new diagram, delete old diagram and fix db

--- SQLandEFCore/01. SQL Fundamentals/Database.sql
+++ SQLandEFCore/01. SQL Fundamentals/Database.sql
@@ -32,12 +32,15 @@ CREATE TABLE [Locations](-- office floor 1 / office floor 2
 	[Latitude] NVARCHAR(20) NOT NULL,
 	[Longitude] NVARCHAR(20) NOT NULL,
 	[Number] INT,
-	[ZIPCode] NVARCHAR(100) NOT NULL
-)
+	[ZIPCode] NVARCHAR(100) NOT NULL,
+	[MaxCapacity] INT NOT NULL
+)  
+
 
 CREATE TABLE [Schedules](
 	[Id] INT PRIMARY KEY IDENTITY,
 	[Date] DATETIME NOT NULL,
+	[Capacity] INT NOT NULL,
 	[UserId] INT NOT NULL FOREIGN KEY REFERENCES [Users]([Id]), --Trainer
 	[LocationId] INT NOT NULL FOREIGN KEY REFERENCES [Locations]([Id]) -- office floor 2
 )
@@ -50,13 +53,12 @@ CREATE TABLE [WorkoutCategories](
 
 CREATE TABLE [Workouts](
 	[Id] INT PRIMARY KEY IDENTITY,
-	[Title] NVARCHAR(100) NOT NULL,
+	[Title] NVARCHAR(100) NOT NULL, -- yoga, mladost 1, bl.
 	[Description] NVARCHAR(1000) NOT NULL,
 	[EquipmentNeeded] NVARCHAR(100),
 	[Duration] DATETIME NOT NULL,
 	[Gender] NVARCHAR(20) NOT NULL,
 	[IntensityLevel] INT NOT NULL,
-	[MaxCapacity] INT NOT NULL,
 	[Status] NVARCHAR NOT NULL,
 	[Picture] NVARCHAR(200),
 	[Price] MONEY NOT NULL,
@@ -82,34 +84,19 @@ CREATE TABLE [Tags](
 
 CREATE TABLE [Bookings](
 	[Id] INT PRIMARY KEY IDENTITY,
-	[CountPeople] INT NOT NULL,
-	[WorkoutId] INT NOT NULL FOREIGN KEY REFERENCES [Workouts]([Id]),
-	[LocationId] INT NOT NULL FOREIGN KEY REFERENCES [Locations]([Id]),
+	--[WorkoutId] INT NOT NULL FOREIGN KEY REFERENCES [Workouts]([Id]),
 	[ScheduleId] INT NOT NULL FOREIGN KEY REFERENCES [Schedules]([Id]),
 	[UserId] INT NOT NULL FOREIGN KEY REFERENCES [Users]([Id])
 )
 
-CREATE TABLE [BookingsUsersMapping](
-	[BookingId] INT NOT NULL FOREIGN KEY REFERENCES [Bookings]([Id]),
-	[UserId] INT NOT NULL FOREIGN KEY REFERENCES [Users]([Id]),
-    PRIMARY KEY ([BookingId], [UserId])
-)
 
-
---WorkoutTrainersMapping
-CREATE TABLE [WorkoutsUsersMapping](
+CREATE TABLE [WorkoutsTrainersMapping](
 	[WorkoutId] INT NOT NULL FOREIGN KEY REFERENCES [Workouts]([Id]),
 	[UserId] INT NOT NULL FOREIGN KEY REFERENCES [Users]([Id]),
     PRIMARY KEY ([WorkoutId], [UserId])
 )
 
-CREATE TABLE [WorkoutsSchedulesMapping](
-	[WorkoutId] INT NOT NULL FOREIGN KEY REFERENCES [Workouts]([Id]),
-	[ScheduleId] INT NOT NULL FOREIGN KEY REFERENCES [Schedules]([Id]),
-    PRIMARY KEY ([WorkoutId], [ScheduleId])
-)
-
-CREATE TABLE [LocationsWorkoutsMapping](
+CREATE TABLE [WorkoutsLocationsMapping](
 	[WorkoutId] INT NOT NULL FOREIGN KEY REFERENCES [Workouts]([Id]),
 	[LocationId] INT NOT NULL FOREIGN KEY REFERENCES [Locations]([Id]),
     PRIMARY KEY ([WorkoutId], [LocationId])
